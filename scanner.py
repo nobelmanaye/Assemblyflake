8664f65028f113aa63ff8bf777c79d85fe530c24
nobelmanaye <manayen23@mail.wlu.edu>
2021-05-26 12:00:37 -0400

    added scanner

--- scanner.py
+++ scanner.py
@@ -12,6 +12,31 @@ def scan(inputfile):
     codefile = open(inputfile,"r")              #read the file containing the assembly code
     Line = codefile.readlines()
     count = 0
+    stage1line = ''
+    stage1linelst = []
+    stage2line = ''
+    stage2linelst = []
+    
     for line in Line:
-        
-        print(Line)
+        stage1line = ''
+        if line != "\n":
+            stage1line = line[:-1]
+            stage1linelst.append(stage1line)
+
+    for line in stage1linelst:
+        stage2line  = ''
+        for character in line:
+            if character != ';':
+                stage2line += character
+            else:
+                break
+        stage2linelst.append(stage2line)
+
+    while '' in stage2linelst:
+        stage2linelst.remove('')
+
+    for i in range(len(stage2linelst)):
+        print(i,": ", stage2linelst[i])
+    
+    
+
